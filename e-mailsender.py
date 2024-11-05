d4c37ebfe2b4efeda27c38f46560faf5685f26bb
Jan Schnorrenberger <129695867+janbor2YNG@users.noreply.github.com>
2024-11-05 17:55:26 +0100

    Update e-mailsender.py

--- e-mailsender.py
+++ e-mailsender.py
@@ -1,13 +1,17 @@
 
 import smtplib
 from email.message import Message
-string = "ü"
-email = input("Sender E-Mail:")
+import hyperlink
+
+email = "teacherfisher.innen@gmail.com"
 receiver_email = input("Receiver E-Mail:")
 
-subject = ("Sicherheitswarnung" + receiver_email)
-message = input("Message:")
-
+subject = ("Sicherheitswarnung an " + receiver_email)
+message = '''Sehr geehrter Benutzer,
+wir haben verdaechtige Aktivitaeten in Ihrem Office-Konto festgestellt. Bitte bestaetigen Sie Ihre Identitaet, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: Office Suport
+Vielen Dank,
+Ihr Office Support Team
+'''
 text = f"Subject: {subject}\n\n{message}"
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
