5eb7a4af3cec1f924c8690cdaf337fda338158c9
Jan Schnorrenberger <129695867+janbor2YNG@users.noreply.github.com>
2024-11-06 14:45:16 +0100

    Update e-mailsender.py
    Codirung für  utf 8

--- e-mailsender.py
+++ e-mailsender.py
@@ -1,32 +1,30 @@
-import smtplib
-from email.message import Message
-import hyperlink
 import random
+import smtplib
+from email.mime.text import MIMEText
 
 name = []
 
 Office1 = '''Sehr geehrter Benutzer,
-wir haben verdaechtige Aktivitäten in Ihrem Office-Konto festgestellt. Bitte bestaetigen Sie Ihre Identitaet, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: Office Suport
+wir haben verdächtige Aktivitäten in Ihrem Office-Konto festgestellt. Bitte bestätigen Sie Ihre Identität, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: Office Support
 Vielen Dank,
 Ihr Office Support Team
 '''
 
 Office2 = '''Hallo,
-um die Sicherheit Ihres Office-Kontos zu gewaehrleisten, muessen wir Ihre Kontoinformationen aktualisieren. Klicken Sie bitte hier: [Link]
-Mit freundlichen Grueßen,
+um die Sicherheit Ihres Office-Kontos zu gewährleisten, müssen wir Ihre Kontoinformationen aktualisieren. Klicken Sie bitte hier: [Link]
+Mit freundlichen Grüßen,
 Office Support
 '''
 
 Office3 = '''Lieber Kunde,
-Ihr Office 365-Abonnement wird in 3 Tagen ablaufen. Bitte verlaengern Sie es sofort, um Unterbrechungen zu vermeiden: [Link]
+Ihr Office 365-Abonnement wird in 3 Tagen ablaufen. Bitte verlängern Sie es sofort, um Unterbrechungen zu vermeiden: [Link]
 Danke,
 Office Support
 '''
 
 mailText = [Office3, Office2, Office1]
 
-
-#Hier wird die email adresse ausgewählt
+# Hier wird die E-Mail-Adresse ausgewählt
 with open('Name.txt') as f:
     name = [line.rstrip() for line in f]
 
@@ -34,23 +32,25 @@ RandomName = random.choice(name)
 
 print(RandomName)
 
-
-#hier wird die email versendet
+# E-Mail-Adresse und Betreff definieren
 email = "teacherfisher.innen@gmail.com"
-#receiver_email = input("Receiver E-Mail:")
+subject = f"Sicherheitswarnung an {RandomName}"
+message = random.choice(mailText)
 
-subject = ("Sicherheitswarnung an " + RandomName)
-message = (random.choice(mailText))
+# MIMEText-Objekt erstellen, das die Kodierung spezifiziert
+mime_message = MIMEText(message, "plain", "utf-8")
+mime_message["From"] = email
+mime_message["To"] = RandomName
+mime_message["Subject"] = subject
 
-text = f"Subject: {subject}\n\n{message}"
-
+# SMTP-Server-Verbindung herstellen und Nachricht senden
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
-server.login(email, "czqc rfzf qijm vvnf ")
+server.login(email, "czqc rfzf qijm vvnf")
 
-
-server.sendmail(email, RandomName, text)
+# Nachricht senden (nur `mime_message.as_string()` verwenden)
+server.sendmail(email, RandomName, mime_message.as_string())
 
 print(message)
 print("Email sent to " + RandomName)
