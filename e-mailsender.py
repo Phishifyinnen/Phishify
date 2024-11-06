9d93aab7d71125b6b8024f457baa673f31010ee2
Hannes Urban <168209661+HannesMarek@users.noreply.github.com>
2024-11-06 14:12:00 +0100

    Update e-mailsender.py

--- e-mailsender.py
+++ e-mailsender.py
@@ -1,17 +1,32 @@
-
 import smtplib
 from email.message import Message
 import hyperlink
+import random
 
+name = []
+
+#Hier wird die email adresse ausgewählt
+with open('Name.txt') as f:
+    name = [line.rstrip() for line in f]
+
+RandomName = random.choice(name)
+
+print(RandomName)
+
+
+#hier wird die email versendet
 email = "teacherfisher.innen@gmail.com"
-receiver_email = input("Receiver E-Mail:")
+#receiver_email = input("Receiver E-Mail:")
 
-subject = ("Sicherheitswarnung an " + receiver_email)
-message = '''Sehr geehrter Benutzer,
-wir haben verdaechtige Aktivitaeten in Ihrem Office-Konto festgestellt. Bitte bestaetigen Sie Ihre Identitaet, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: Office Suport
+subject = ("Sicherheitswarnung an " + RandomName)
+message = '''Sehr geehrter Nutzer,
+wir haben verdaechtige Aktivitaeten in Ihrem Google-Konto festgestellt. Bitte bestaetigen Sie Ihre Identitaet, um den Zugriff auf Ihr Konto zu sichern.
+
+Jetzt verifizieren
+
 Vielen Dank,
-Ihr Office Support Team
-'''
+Google Support'''
+
 text = f"Subject: {subject}\n\n{message}"
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -20,6 +35,7 @@ server.starttls()
 server.login(email, "czqc rfzf qijm vvnf ")
 
 
-server.sendmail(email, receiver_email, text)
+server.sendmail(email, RandomName, text)
 
-print("Email sent to " + receiver_email)
+print(message)
+print("Email sent to " + RandomName)
