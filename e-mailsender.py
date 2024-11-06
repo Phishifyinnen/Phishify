66d6c8d892940b27010da9d8daf456bb7a61e321
Jan Schnorrenberger <129695867+janbor2YNG@users.noreply.github.com>
2024-11-06 14:23:18 +0100

    Update e-mailsender.py
    merge

--- e-mailsender.py
+++ e-mailsender.py
@@ -5,6 +5,27 @@ import random
 
 name = []
 
+Office1 = '''Sehr geehrter Benutzer,
+wir haben verdächtige Aktivitäten in Ihrem Office-Konto festgestellt. Bitte bestätigen Sie Ihre Identität, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: Office Suport
+Vielen Dank,
+Ihr Office Support Team
+'''
+
+Office2 = '''Hallo,
+um die Sicherheit Ihres Office-Kontos zu gewaehrleisten, muessen wir Ihre Kontoinformationen aktualisieren. Klicken Sie bitte hier: [Link]
+Mit freundlichen Grüßen,
+Office Support
+'''
+
+Office3 = '''Lieber Kunde,
+Ihr Office 365-Abonnement wird in 3 Tagen ablaufen. Bitte verlängern Sie es sofort, um Unterbrechungen zu vermeiden: [Link]
+Danke,
+Office Support
+'''
+
+mailText = [Office3, Office2, Office1]
+
+
 #Hier wird die email adresse ausgewählt
 with open('Name.txt') as f:
     name = [line.rstrip() for line in f]
@@ -19,13 +40,7 @@ email = "teacherfisher.innen@gmail.com"
 #receiver_email = input("Receiver E-Mail:")
 
 subject = ("Sicherheitswarnung an " + RandomName)
-message = '''Sehr geehrter Nutzer,
-wir haben verdaechtige Aktivitaeten in Ihrem Google-Konto festgestellt. Bitte bestaetigen Sie Ihre Identitaet, um den Zugriff auf Ihr Konto zu sichern.
-
-Jetzt verifizieren
-
-Vielen Dank,
-Google Support'''
+message = (random.choice(messagteText))
 
 text = f"Subject: {subject}\n\n{message}"
 
