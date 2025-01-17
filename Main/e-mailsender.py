85b5f2a8b65929899fc20591d897051fba647445
Jan Schnorrenberger <129695867+janbor2YNG@users.noreply.github.com>
2025-01-17 18:26:11 +0100

    Update e-mailsender.py
    e-mail updated

--- Main/e-mailsender.py
+++ Main/e-mailsender.py
@@ -26,16 +26,31 @@ with open('output.txt', 'r', encoding='utf-8') as f:
 
 
 # Beispielnachrichten im HTML-Format
-Office1 = '''<html>
+Google = f'''<html>
+</style><link href=3D"//fonts.googlea=pis.com/css?family=3DGoogle+Sans" rel=3D"stylesheet" type=3D"text/css"
+              nonce='e=3D"a5JpJkBI2Cl2HVmUu1etdg"/'>
 <head> <meta charset="UTF-8"></head>
 <body>
-<div style="width: 500px; height: 450px; border-radius: 10px; border-width: thin; border-style: solid; display: grid; place-content: center; border-color: #dadce0; margin-inside: 35px">
-    <p style="font-size: 14px; color: rgba(0,0,0,1); line-height: 1.6; margin: 10px">Sehr geehrter Benutzer,</p>
-    <p style="font-size: 14px; color: rgba(0,0,0,1); line-height: 1.6; margin: 10px">wir haben verdächtige Aktivitäten in Ihrem Office-Konto festgestellt. Bitte bestätigen Sie Ihre Identität.</p>
-    <p>Danke,<br>Office Support</p>
+<div style="width: 500px; height: 500px; border-radius: 10px; border-width: thin; border-style: solid; display: grid; place-content: center; border-color: #dadce0; margin-inside: 35px">
+   <div style="margin: auto; display: flex; align-items: center; height: 30px;width: 80px;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/250px-Google_2015_logo.svg.png" alt="https://www.itespresso.de/wp-content/uploads/2015/03/ms-office-logo-800-684x513.jpg" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/250px-Google_2015_logo.svg.png" style="width: 100%; max-width: 100px; display: block; margin: 0 auto;">
+   </div>
+    <p style="text-align: center; font-family:Roboto-Regular,Helvetica,Arial; font-size: 25px; margin: auto; margin-top: 2px; margin-bottom: 2px;">Sicherheitswarnung für <br></p>
+    <p style="text-align: center; font-family:Roboto-Regular,Helvetica,Arial; font-size: 12px; color: rgba(0,0,0,1); line-height: 1.6;">{email}</p>
+    <div style="text-align: center; margin: 15px; font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:14px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px; border-width: thin; border-color: #dadce0; border-top-style: solid" margin-top: 5px;>
+    <p style="font-size: 14px; color: rgba(0,0,0,1); line-height: 1.6;margin-top:5px; margin: 10px">Sehr geehrter Benutzer,</p>
+    <p style="font-size: 14px; color: rgba(0,0,0,1); line-height: 1.6; margin: 10px">wir haben verdächtige Aktivitäten in Ihrem Office-Konto festgestellt. Bitte bestätigen Sie Ihre Identität, indem Sie auf den folgenden Link klicken und Ihre Daten eingeben: <a href="http://example.com">Office Support</a></p>
+        <button style="background-color: #3981ec; color: white;font-size:14px; height: 35px; width: 135px; border-width: 0px; border-radius: 5px">Aktivität prüfen</button>
+    <p style="font-size: 14px; color: rgba(0,0,0,1); line-height: 1.6; margin: 10px">Vielen Dank,<br>Ihr Office Support Team</p>
+    </div>
+</div>
+<div style="text-align: center; width: 500px">
+    <p style="font-size: 10px; color: gray; margin: 10px; margin-bottom: 0px">Diese E-Mail wurde automatisch generiert. Sollten Sie Probleme haben, wenden Sie sich bitte <br>an unseren Kundenservice.</p>
+    <p style="font-size: 10px; color: gray; margin: 10px; margin-top: 2.5px">Ⓓ 2024 Office United States of America Ltd., Molton House, Hindstons Street, Antonstans 41, USA</p>
 </div>
 </body>
 </html>
+
 '''
 
 Office2 = f'''<html>
@@ -71,7 +86,7 @@ Office3 = '''<html>
 '''
 
 # Alle Nachrichten in einer Liste
-mailText = [Office1]
+mailText = [Google]
 
 # Zeitintervall festlegen (Sekunden oder Minuten)
 # Hier: Intervall zwischen den E-Mails in Sekunden (60 = 1 Minute)
@@ -87,7 +102,7 @@ server.login("teacherfisher.innen@gmail.com", "czqc rfzf qijm vvnf")
 for i in range(0, haeufigkeit):
     # Betreff definieren und Nachricht auswählen
     subject = f"Sicherheitswarnung an {name} {lastname}"
-    message = Office1
+    message = Google
 
     # MIMEMultipart-Objekt erstellen
     mime_message = MIMEMultipart()
